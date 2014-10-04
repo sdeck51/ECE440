e3957c2ccd2a928228815da17bc78799e55a18d0
rad- <steven.t.seppala@gmail.com>
2014-10-03 21:35:05 -0600

    Got somehting working?

--- socketlab/project.py
+++ socketlab/project.py
@@ -1,5 +1,5 @@
 from socket import *
-
+quit = "q"
 server = socket(AF_INET, SOCK_STREAM)
 port = 12030
 server.bind((gethostname(), port))
@@ -9,8 +9,18 @@ while True:
     conection,  addr = server.accept()
     try:
         print 'Working'
+        message = 'OK HellowWorld.html'
+        filename = message.split()[1]
+        f = open(filename[1:])
+        close(filename[1:])
+        print filename
         conection.close()
     except IOError:
         print 'Something happened kinda badly'
         connection.close()
         server.close()
+    except KeyboardInterrupt:
+        break;
+
+
+#threads set to daemon so no stdin capture
