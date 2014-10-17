c2a3a43062cc043cb642b2875cb4c79bf80e3707
sdeck51 <sdeck51@unm.edu>
2014-10-16 18:46:13 -0600

    Update threadServer.py
    works

--- socketlab/threadServer.py
+++ socketlab/threadServer.py
@@ -2,30 +2,30 @@ from socket import *
 import threading
 
 class newclient(threading.Thread):
-    def __init__(self, addr,conection):
+    def __init__(self, addr,connection):
         threading.Thread.__init__(self)
         self.addr = addr
-        self.socket = conection
+        self.socket = connection
         print "New thread Made\n"
     def run(self):
         print "Connecting to" + str(addr)
 	try:
 		print 'Working'
-		conection.send("HTTP /1.1 200 OK\r\n")
-		message = conection.recv(1024)
+		connection.send("HTTP/1.1 200 OK\r\n\r\n")
+		message = connection.recv(1024)
 		filename = message.split()[1] #cuts off the '/' in the request page
 		f = open(filename[1:])
 		outputdata = f.read()
 		for i in range(0, len(outputdata)):
-			conection.send(outputdata[i])
-		conection.close()
+			connection.send(outputdata[i])
+		connection.close()
 	except IOError:
 		print 'IO ERROR'
-		conection.send("404 NOT FOUND")
-		conection.close()
+		connection.send("404 NOT FOUND")
+		connection.close()
 	except KeyboardInterrupt:
 		server.close()
-		conection.close()
+		connection.close()
 		exit();
 
 
@@ -37,8 +37,8 @@ server.bind((gethostname(), port))
 while True:
     server.listen(5) 
     print 'Ready to serve'
-    conection,  addr = server.accept()
-    newthread = newclient(addr,conection)
+    connection,  addr = server.accept()
+    newthread = newclient(addr,connection)
     newthread.start()
 #threads set to daemon so no stdin capture
 #in browser open page with '"computer_name":12030'
